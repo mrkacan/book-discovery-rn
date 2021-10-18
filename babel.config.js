5008b42d54a7d608b93f7596143a5e3280732290
Emre Kaçan <emrekacan@gmail.com>
2021-10-18 11:47:14 +0300

    Removed .env functionality.

--- babel.config.js
+++ babel.config.js
@@ -2,16 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin', [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-        blacklist: null,
-        whitelist: null,
-        safe: false,
-        allowUndefined: false,
-      },
-    ],],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
